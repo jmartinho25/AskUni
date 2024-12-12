bef7fa06366516dce33671b61e53f6e5bd5355eb
leonardomagalhaes21 <leonardomagalhaess21@gmail.com>
2024-12-12 23:10:07 +0000

    added 1 tooltip

--- resources/views/emails/form.blade.php
+++ resources/views/emails/form.blade.php
@@ -4,7 +4,12 @@
 <div class="container">
     <form action="{{ route('send.email') }}" method="POST">
         @csrf
-        <label for="email">Email:</label>
+        <label for="email">Email
+            <div class="tooltip">
+                    <i class="fas fa-info-circle"></i>
+                    <span class="tooltip-text">Format: @fe.up.pt</span>
+            </div>
+        </label>
         <input type="email" name="email" id="email" required>
         <button type="submit">Send</button>
     </form>
